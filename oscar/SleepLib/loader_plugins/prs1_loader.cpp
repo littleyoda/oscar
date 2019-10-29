littleyoda/oscar

commit 9a25647c252f5a581c951db284d202da068d826f

Add initial support for PRS1 settings channel (as distinct from statistics).
Right now this results in slightly confusing graphs, since sometimes
the IPAP/EPAP data for a machine is primarily reported by its settings
events, and for other machines by its statistics events. Also, the
"average" pressure on F0 looks like it might be effectivley EPAP in
single-pressure mode rather than the true average in bi-level mode.

Once we decide on the best presentation, we can either update the importer
or the display to show the most helpful channels for a given session.

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -1563,12 +1563,12 @@ static const QHash<PRS1ParsedEventType,QVector<ChannelID*>> PRS1ImportChannelMap
     // F0 imports pressure-set events and ignores pressure average events.
     // F5 imports average events and ignores EPAP set events.
     // F3 imports average events and has no set events.
-    { PRS1PressureSetEvent::TYPE,       { &CPAP_Pressure } },
-    { PRS1IPAPSetEvent::TYPE,           { &CPAP_IPAP } },
-    { PRS1EPAPSetEvent::TYPE,           { &CPAP_EPAP, &CPAP_PS } },  // PS is calculated from IPAP and EPAP
-    { PRS1PressureAverageEvent::TYPE,   { /*&CPAP_Pressure*/ } },  // TODO: not currently imported, so don't create the channel
+    { PRS1PressureSetEvent::TYPE,       { &CPAP_PressureSet } },
+    { PRS1IPAPSetEvent::TYPE,           { &CPAP_IPAPSet, &CPAP_PS } },  // PS is calculated from IPAPset and EPAPset when both are supported (F0) TODO: Should this be a separate channel since it's not a 2-minute average?
+    { PRS1EPAPSetEvent::TYPE,           { &CPAP_EPAPSet } },            // EPAPset is supported on F5 without any corresponding IPAPset, so it shouldn't always create a PS channel
+    { PRS1PressureAverageEvent::TYPE,   { &CPAP_Pressure } },
     { PRS1IPAPAverageEvent::TYPE,       { &CPAP_IPAP } },
-    { PRS1EPAPAverageEvent::TYPE,       { &CPAP_EPAP, &CPAP_PS } },  // PS is calculated from IPAP and EPAP
+    { PRS1EPAPAverageEvent::TYPE,       { &CPAP_EPAP, &CPAP_PS } },     // PS is calculated from IPAP and EPAP averages (F3 and F5)
     { PRS1IPAPLowEvent::TYPE,           { &CPAP_IPAPLo } },
     { PRS1IPAPHighEvent::TYPE,          { &CPAP_IPAPHi } },
 
@@ -2627,11 +2627,17 @@ bool PRS1Import::ImportEventChunk(PRS1DataChunk* event)
 {
     EventDataType currentPressure=0;
 
+    const QVector<PRS1ParsedEventType> & supported = GetSupportedEvents(event);
+    
+    // Calculate PS from IPAP/EPAP set events only when both are supported. This includes F0, but excludes
+    // F5, which only reports EPAP set events, but both IPAP/EPAP average, from which PS will be calculated.
+    bool calcPSfromSet = supported.contains(PRS1IPAPSetEvent::TYPE) && supported.contains(PRS1EPAPSetEvent::TYPE);
+    
     // Unintentional leak calculation, see zMaskProfile:calcLeak in calcs.cpp for explanation
     bool calcLeaks = p_profile->cpap->calculateUnintentionalLeaks();
     if (calcLeaks) {
         // Only needed for machines that don't support it directly.
-        calcLeaks = (GetSupportedEvents(event).contains(PRS1LeakEvent::TYPE) == false);
+        calcLeaks = (supported.contains(PRS1LeakEvent::TYPE) == false);
     }
     EventDataType lpm4 = p_profile->cpap->custom4cmH2OLeaks();
     EventDataType lpm20 = p_profile->cpap->custom20cmH2OLeaks();
@@ -2661,30 +2667,21 @@ bool PRS1Import::ImportEventChunk(PRS1DataChunk* event)
         switch (e->m_type) {
             case PRS1UnknownDurationEvent::TYPE:  // TODO: We should import and graph this as PRS1_0E
                 break;  // not imported or displayed
-            case PRS1PressureSetEvent::TYPE:
-                AddEvent(channel, t, e->m_value, e->m_gain);
-                currentPressure = e->m_value;
-                break;
             case PRS1IPAPSetEvent::TYPE:
+            case PRS1IPAPAverageEvent::TYPE:
                 AddEvent(channel, t, e->m_value, e->m_gain);
                 currentPressure = e->m_value;
                 break;
             case PRS1EPAPSetEvent::TYPE:
-                if (event->family == 5) break;  // TODO: Once there are separate average and setting channels, this special-case can be removed.
-                PS = *channels.at(1);
                 AddEvent(channel, t, e->m_value, e->m_gain);
-                AddEvent(PS, t, currentPressure - e->m_value, e->m_gain);  // Pressure Support
-                break;
-            case PRS1PressureAverageEvent::TYPE:
-                // TODO, we need OSCAR channels for average stats
-                break;
-            case PRS1IPAPAverageEvent::TYPE:
-                AddEvent(channel, t, e->m_value, e->m_gain);  // TODO: This belongs in an average channel rather than setting channel.
-                currentPressure = e->m_value;
+                if (calcPSfromSet) {
+                    PS = *(PRS1ImportChannelMap[PRS1IPAPSetEvent::TYPE].at(1));
+                    AddEvent(PS, t, currentPressure - e->m_value, e->m_gain);  // Pressure Support
+                }
                 break;
             case PRS1EPAPAverageEvent::TYPE:
                 PS = *channels.at(1);
-                AddEvent(channel, t, e->m_value, e->m_gain);  // TODO: This belongs in an average channel rather than setting channel.
+                AddEvent(channel, t, e->m_value, e->m_gain);
                 AddEvent(PS, t, currentPressure - e->m_value, e->m_gain);  // Pressure Support
                 break;
 

oscar/SleepLib/machine_common.cpp
@@ -23,7 +23,8 @@ ChannelID CPAP_IPAP, CPAP_IPAPLo, CPAP_IPAPHi, CPAP_EPAP, CPAP_EPAPLo, CPAP_EPAP
           CPAP_LeakMedian, CPAP_LeakTotal, CPAP_MaxLeak, CPAP_FLG, CPAP_IE, CPAP_Te, CPAP_Ti, CPAP_TgMV,
           CPAP_UserFlag1, CPAP_UserFlag2, CPAP_UserFlag3, CPAP_BrokenSummary, CPAP_BrokenWaveform, CPAP_RDI,
           CPAP_PresReliefMode, CPAP_PresReliefLevel, CPAP_PSMin, CPAP_PSMax, CPAP_Test1,
-          CPAP_Test2, CPAP_HumidSetting;
+          CPAP_Test2, CPAP_HumidSetting,
+          CPAP_PressureSet, CPAP_IPAPSet, CPAP_EPAPSet;
 
 
 ChannelID RMS9_E01, RMS9_E02, RMS9_SetPressure, RMS9_MaskOnTime;

oscar/SleepLib/machine_common.h
@@ -138,7 +138,8 @@ extern ChannelID CPAP_IPAP, CPAP_IPAPLo, CPAP_IPAPHi, CPAP_EPAP, CPAP_EPAPLo, CP
        CPAP_RespEvent, CPAP_Snore, CPAP_MinuteVent, CPAP_RespRate, CPAP_TidalVolume, CPAP_PTB, CPAP_Leak,
        CPAP_LeakMedian, CPAP_LeakTotal, CPAP_MaxLeak, CPAP_FLG, CPAP_IE, CPAP_Te, CPAP_Ti, CPAP_TgMV,
        CPAP_UserFlag1, CPAP_UserFlag2, CPAP_UserFlag3, CPAP_BrokenSummary, CPAP_BrokenWaveform, CPAP_RDI,
-       CPAP_PresReliefMode, CPAP_PresReliefLevel, CPAP_Test1, CPAP_Test2;
+       CPAP_PresReliefMode, CPAP_PresReliefLevel, CPAP_Test1, CPAP_Test2,
+       CPAP_PressureSet, CPAP_IPAPSet, CPAP_EPAPSet;
 
 extern ChannelID RMS9_E01, RMS9_E02, RMS9_SetPressure, RMS9_MaskOnTime;
 extern ChannelID PRS1_00, PRS1_01, PRS1_08, PRS1_0A, PRS1_0B, PRS1_0C, PRS1_0E, PRS1_0F, CPAP_LargeLeak,

oscar/tests/sessiontests.cpp
@@ -139,6 +139,9 @@ static QString eventChannel(ChannelID i)
         CHANNELNAME(PRS1_0C);
         CHANNELNAME(PRS1_0E);
         CHANNELNAME(PRS1_15);
+        CHANNELNAME(CPAP_PressureSet);
+        CHANNELNAME(CPAP_IPAPSet);
+        CHANNELNAME(CPAP_EPAPSet);
         s = hex(i);
         qDebug() << "event channel" << qPrintable(s);
     } while(false);

oscar/Graphs/gLineChart.cpp
@@ -1,5 +1,6 @@
-﻿/* gLineChart Implementation
+/* gLineChart Implementation
  *
+ * Copyright (c) 2019 The OSCAR Team
  * Copyright (c) 2011-2018 Mark Watkins <mark@jedimark.net>
  *
  * This file is subject to the terms and conditions of the GNU General Public
@@ -348,11 +349,11 @@ QString gLineChart::getMetaString(qint64 time)
             val = m_day->lookupValue(code, time, m_square_plot);
             lasttext += " "+QString("%1: %2").arg(schema::channel[code].label()).arg(val,0,'f',2); //.arg(schema::channel[code].units());
 
-            if (code == CPAP_IPAP) {
+            if (code == CPAP_IPAP || code == CPAP_IPAPSet) {
                 ipap = val;
                 addPS = true;
             }
-            if (code == CPAP_EPAP) {
+            if (code == CPAP_EPAP || code == CPAP_EPAPSet) {
                 epap = val;
             }
         }
@@ -526,7 +527,10 @@ void gLineChart::paint(QPainter &painter, gGraph &w, const QRegion &region)
 
     float lineThickness = AppSetting->lineThickness()+0.001F;
 
-    for (const auto & code : m_codes) {
+    for (int ic = 0; ic < m_codes.count(); ic++) {
+        const auto & code = m_codes[ic];
+        square_plot = m_square[ic];  // set the mode per-channel
+
         const schema::Channel &chan = schema::channel[code];
 
         ////////////////////////////////////////////////////////////////////////
@@ -613,7 +617,7 @@ void gLineChart::paint(QPainter &painter, gGraph &w, const QRegion &region)
 
                 if (m_disable_accel) { accel = false; }
 
-                square_plot = m_square_plot;
+                //square_plot = m_square_plot;  // now we set this per-channel above
 
                 if (accel || num_points > 20000) { // Don't square plot if too many points or waveform
                     square_plot = false;

oscar/SleepLib/schema.cpp
@@ -1,5 +1,6 @@
-﻿/* Channel / Schema Implementation
+/* Channel / Schema Implementation
  *
+ * Copyright (c) 2019 The OSCAR Team
  * Copyright (c) 2011-2018 Mark Watkins <mark@jedimark.net>
  *
  * This file is subject to the terms and conditions of the GNU General Public
@@ -134,6 +135,9 @@ void init()
     schema::channel.add(GRP_CPAP, new Channel(CPAP_RampTime      = 0x1022, SETTING,     MT_CPAP, SESSION, "RampTime",       QObject::tr("Ramp Time") ,      QObject::tr("Ramp Delay Period"),              QObject::tr("Ramp Time"),    STR_UNIT_Minutes,      DEFAULT,    QColor("black")));
     schema::channel.add(GRP_CPAP, new Channel(CPAP_RampPressure  = 0x1023, SETTING,     MT_CPAP, SESSION, "RampPressure",   QObject::tr("Ramp Pressure"),   QObject::tr("Starting Ramp Pressure"),         QObject::tr("Ramp Pressure"),STR_UNIT_CMH2O,        DEFAULT,    QColor("black")));
     schema::channel.add(GRP_CPAP, new Channel(CPAP_Ramp          = 0x1027, SPAN,        MT_CPAP, SESSION, "Ramp",           QObject::tr("Ramp Event") ,     QObject::tr("Ramp Event"),                     QObject::tr("Ramp"),         STR_UNIT_EventsPerHour,DEFAULT,    QColor("light blue")));
+    schema::channel.add(GRP_CPAP, new Channel(CPAP_PressureSet   = 0x11A1, WAVEFORM,    MT_CPAP, SESSION, "PressureSet",    QObject::tr("Pressure Set"),    QObject::tr("Pressure Setting"),               QObject::tr("Pressure Set"), STR_UNIT_CMH2O,        DEFAULT,    QColor("dark red")));
+    schema::channel.add(GRP_CPAP, new Channel(CPAP_IPAPSet       = 0x11A2, WAVEFORM,    MT_CPAP, SESSION, "IPAPSet",        QObject::tr("IPAP Set"),        QObject::tr("IPAP Setting"),                   QObject::tr("IPAP Set"),     STR_UNIT_CMH2O,        DEFAULT,    QColor("dark red")));
+    schema::channel.add(GRP_CPAP, new Channel(CPAP_EPAPSet       = 0x11A3, WAVEFORM,    MT_CPAP, SESSION, "EPAPSet",        QObject::tr("EPAP Set"),        QObject::tr("EPAP Setting"),                   QObject::tr("EPAP Set"),     STR_UNIT_CMH2O,        DEFAULT,    QColor("dark green")));
     // Flags
     schema::channel.add(GRP_CPAP, new Channel(CPAP_CSR           = 0x1000, SPAN,        MT_CPAP, SESSION, "CSR",
             QObject::tr("Cheyne Stokes Respiration"), QObject::tr("An abnormal period of Cheyne Stokes Respiration"), QObject::tr("CSR"), STR_UNIT_Percentage,DEFAULT,    COLOR_CSR));

oscar/daily.cpp
@@ -349,6 +349,9 @@ Daily::Daily(QWidget *parent,gGraphView * shared)
     pc->addPlot(CPAP_IPAPLo, square);
     pc->addPlot(CPAP_IPAP, square);
     pc->addPlot(CPAP_IPAPHi, square);
+    pc->addPlot(CPAP_PressureSet, false);
+    pc->addPlot(CPAP_EPAPSet, false);
+    pc->addPlot(CPAP_IPAPSet, false);
 
     // Create Timea at Pressure graph
     gGraph * TAP2;

commit de73d3756cb6cc8374b778d5dd5e2cfa63eadaf4

Import and graph unknown PRS1_0E spans.
Also clean up unused PRS1 channels and fix a regression from
9a25647c that broke unintentional leak calculation for CPAP/APAP.

Also add PRS1PressureAverageEvent to F0V4 list of supported events.

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -1579,7 +1579,7 @@ static const QHash<PRS1ParsedEventType,QVector<ChannelID*>> PRS1ImportChannelMap
     { PRS1ApneaAlarmEvent::TYPE,        { /* Not imported */ } },
     { PRS1SnoresAtPressureEvent::TYPE,  { /* Not imported */ } },
     { PRS1AutoPressureSetEvent::TYPE,   { /* Not imported */ } },
-    { PRS1UnknownDurationEvent::TYPE,   { /* Not imported */ } },  // TODO: import as PRS1_0E
+    { PRS1UnknownDurationEvent::TYPE,   { &PRS1_0E } },
 };
 
 //********************************************************************************************
@@ -2665,8 +2665,7 @@ bool PRS1Import::ImportEventChunk(PRS1DataChunk* event)
         }
         
         switch (e->m_type) {
-            case PRS1UnknownDurationEvent::TYPE:  // TODO: We should import and graph this as PRS1_0E
-                break;  // not imported or displayed
+            case PRS1PressureSetEvent::TYPE:  // currentPressure is used to calculate unintentional leak, not just PS
             case PRS1IPAPSetEvent::TYPE:
             case PRS1IPAPAverageEvent::TYPE:
                 AddEvent(channel, t, e->m_value, e->m_gain);
@@ -2703,6 +2702,7 @@ bool PRS1Import::ImportEventChunk(PRS1DataChunk* event)
 
             case PRS1PeriodicBreathingEvent::TYPE:
             case PRS1LargeLeakEvent::TYPE:
+            case PRS1UnknownDurationEvent::TYPE:
                 // TODO: The graphs silently treat the timestamp of a span as an end time rather than start (see gFlagsLine::paint).
                 // Decide whether to preserve that behavior or change it universally and update either this code or comment.
                 duration = e->m_duration * 1000L;
@@ -3351,6 +3351,7 @@ static const QVector<PRS1ParsedEventType> ParsedEventsF0V4 = {
     PRS1LargeLeakEvent::TYPE,
     PRS1TotalLeakEvent::TYPE,
     PRS1SnoreEvent::TYPE,
+    PRS1PressureAverageEvent::TYPE,
     PRS1SnoresAtPressureEvent::TYPE,
 };
 
@@ -7864,58 +7865,14 @@ void PRS1Loader::initChannels()
     QString unknownname=QObject::tr("PRS1_%1");
     QString unknownshort=QObject::tr("PRS1_%1");
 
-    channel.add(GRP_CPAP, new Channel(PRS1_00 = 0x1150, UNKNOWN, MT_CPAP,    SESSION,
-        "PRS1_00",
-        QString(unknownname).arg(0,2,16,QChar('0')),
-        QString(unknowndesc).arg(0,2,16,QChar('0')),
-        QString(unknownshort).arg(0,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-
-    channel.add(GRP_CPAP, new Channel(PRS1_01 = 0x1151, UNKNOWN,  MT_CPAP,   SESSION,
-        "PRS1_01",
-        QString(unknownname).arg(1,2,16,QChar('0')),
-        QString(unknowndesc).arg(1,2,16,QChar('0')),
-        QString(unknownshort).arg(1,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-
-    channel.add(GRP_CPAP, new Channel(PRS1_08 = 0x1152, UNKNOWN, MT_CPAP,    SESSION,
-        "PRS1_08",
-        QString(unknownname).arg(8,2,16,QChar('0')),
-        QString(unknowndesc).arg(8,2,16,QChar('0')),
-        QString(unknownshort).arg(8,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-
-    channel.add(GRP_CPAP, new Channel(PRS1_0A = 0x1154, UNKNOWN, MT_CPAP,    SESSION,
-        "PRS1_0A",
-        QString(unknownname).arg(0xa,2,16,QChar('0')),
-        QString(unknowndesc).arg(0xa,2,16,QChar('0')),
-        QString(unknownshort).arg(0xa,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-    channel.add(GRP_CPAP, new Channel(PRS1_0B = 0x1155, UNKNOWN,  MT_CPAP,   SESSION,
-        "PRS1_0B",
-        QString(unknownname).arg(0xb,2,16,QChar('0')),
-        QString(unknowndesc).arg(0xb,2,16,QChar('0')),
-        QString(unknownshort).arg(0xb,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-    channel.add(GRP_CPAP, new Channel(PRS1_0C = 0x1156, UNKNOWN,  MT_CPAP,   SESSION,
-        "PRS1_0C",
-        QString(unknownname).arg(0xc,2,16,QChar('0')),
-        QString(unknowndesc).arg(0xc,2,16,QChar('0')),
-        QString(unknownshort).arg(0xc,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-    channel.add(GRP_CPAP, new Channel(PRS1_0E = 0x1157, UNKNOWN, MT_CPAP,    SESSION,
+    channel.add(GRP_CPAP, new Channel(PRS1_0E = 0x1157, SPAN, MT_CPAP,    SESSION,
         "PRS1_0E",
-        QString(unknownname).arg(0xe,2,16,QChar('0')),
+        "??",  // QString(unknownname).arg(0xe,2,16,QChar('0')),
         QString(unknowndesc).arg(0xe,2,16,QChar('0')),
-        QString(unknownshort).arg(0xe,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
+        "??",
+        STR_UNIT_Percentage,
+        DEFAULT,    QColor("#ffe8f0")));
+    qDebug() << channel[PRS1_0E].defaultColor();
     channel.add(GRP_CPAP, new Channel(PRS1_BND = 0x1159, SPAN,  MT_CPAP,   SESSION,
         "PRS1_BND",
         QObject::tr("Breathing Not Detected"),
@@ -7923,16 +7880,6 @@ void PRS1Loader::initChannels()
         QObject::tr("BND"),
         STR_UNIT_Unknown,
         DEFAULT,    QColor("light purple")));
-
-    channel.add(GRP_CPAP, new Channel(PRS1_15 = 0x115A, UNKNOWN,  MT_CPAP,   SESSION,
-        "PRS1_15",
-        QString(unknownname).arg(0x15,2,16,QChar('0')),
-        QString(unknowndesc).arg(0x15,2,16,QChar('0')),
-        QString(unknownshort).arg(0x15,2,16,QChar('0')),
-        STR_UNIT_Unknown,
-        DEFAULT,    QColor("black")));
-
-
     channel.add(GRP_CPAP, new Channel(PRS1_TimedBreath = 0x1180, MINOR_FLAG, MT_CPAP,    SESSION,
         "PRS1TimedBreath",
         QObject::tr("Timed Breath"),

oscar/SleepLib/machine_common.cpp
@@ -30,7 +30,7 @@ ChannelID CPAP_IPAP, CPAP_IPAPLo, CPAP_IPAPHi, CPAP_EPAP, CPAP_EPAPLo, CPAP_EPAP
 ChannelID RMS9_E01, RMS9_E02, RMS9_SetPressure, RMS9_MaskOnTime;
 ChannelID INTELLIPAP_Unknown1, INTELLIPAP_Unknown2;
 
-ChannelID PRS1_00, PRS1_01, PRS1_08, PRS1_0A, PRS1_0B, PRS1_0C, PRS1_0E, PRS1_0F, CPAP_LargeLeak, PRS1_12, PRS1_13, PRS1_15,
+ChannelID PRS1_0E, CPAP_LargeLeak,
           PRS1_BND, PRS1_FlexMode, PRS1_FlexLevel, PRS1_HumidStatus, PRS1_HumidLevel, PRS1_SysLock,
           PRS1_SysOneResistStat,
           PRS1_SysOneResistSet, PRS1_HoseDiam, PRS1_AutoOn, PRS1_AutoOff, PRS1_MaskAlert, PRS1_ShowAHI;

oscar/SleepLib/machine_common.h
@@ -142,8 +142,7 @@ extern ChannelID CPAP_IPAP, CPAP_IPAPLo, CPAP_IPAPHi, CPAP_EPAP, CPAP_EPAPLo, CP
        CPAP_PressureSet, CPAP_IPAPSet, CPAP_EPAPSet;
 
 extern ChannelID RMS9_E01, RMS9_E02, RMS9_SetPressure, RMS9_MaskOnTime;
-extern ChannelID PRS1_00, PRS1_01, PRS1_08, PRS1_0A, PRS1_0B, PRS1_0C, PRS1_0E, PRS1_0F, CPAP_LargeLeak,
-       PRS1_12, PRS1_13, PRS1_15, PRS1_BND,
+extern ChannelID PRS1_0E, CPAP_LargeLeak, PRS1_BND,
        PRS1_FlexMode, PRS1_FlexLevel, PRS1_HumidStatus, PRS1_HumidLevel, CPAP_HumidSetting, PRS1_SysLock,
        PRS1_SysOneResistStat,
        PRS1_SysOneResistSet, PRS1_HoseDiam, PRS1_AutoOn, PRS1_AutoOff, PRS1_MaskAlert, PRS1_ShowAHI;

oscar/tests/sessiontests.cpp
@@ -131,14 +131,7 @@ static QString eventChannel(ChannelID i)
         CHANNELNAME(CPAP_Test2);
         CHANNELNAME(CPAP_PressurePulse);
         CHANNELNAME(CPAP_Pressure);
-        CHANNELNAME(PRS1_00);
-        CHANNELNAME(PRS1_01);
-        CHANNELNAME(PRS1_08);
-        CHANNELNAME(PRS1_0A);
-        CHANNELNAME(PRS1_0B);
-        CHANNELNAME(PRS1_0C);
         CHANNELNAME(PRS1_0E);
-        CHANNELNAME(PRS1_15);
         CHANNELNAME(CPAP_PressureSet);
         CHANNELNAME(CPAP_IPAPSet);
         CHANNELNAME(CPAP_EPAPSet);