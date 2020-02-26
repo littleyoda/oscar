7839af548bdef9d554de7594130cc1705bbaf968
Phil Olynyk <phil@pholynyk.ca>
2020-02-26 11:16:04 -0500

    Remove obsolete version script

--- version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-version=`cat ./oscr/version.h | perl -ne 'my @array = /^const\s*int (major|minor|revision)_(version|number) = (\d+;).*/; print $array[2]' | perl -ne 'my @ar=split(";"); print join(".", @ar);'` 
-buildno=`cat ./oscr/scripts/build_number`
-gitversion=`git rev-parse --short HEAD`
-echo "OSCR Source Information"
-echo "Current Version: v${version}-${buildno} (git rev ${gitversion})"
-echo "Last commit: `git log -1 --format=%cd`"
-echo "Use 'git pull' to get up to date"
