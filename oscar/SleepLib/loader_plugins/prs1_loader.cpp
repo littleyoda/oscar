littleyoda/oscar

commit cb576437abeecf0901715c66ff9723d2408c1f3b

Clean up noise and memory leaks in unit tests.

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -8700,7 +8700,6 @@ void PRS1Loader::initChannels()
         "??",
         STR_UNIT_Seconds,
         DEFAULT,    QColor("#ffe8f0")));
-    qDebug() << channel[PRS1_0E].defaultColor();
     channel.add(GRP_CPAP, new Channel(PRS1_BND = 0x1159, SPAN,  MT_CPAP,   SESSION,
         "PRS1_BND",
         QObject::tr("Breathing Not Detected"),

oscar/SleepLib/schema.cpp
@@ -359,13 +359,22 @@ void init()
 }
 
 
-void resetChannels()
+void done()
 {
-    schema::channel.channels.clear();
     schema::channel.names.clear();
+    for (auto & c : schema::channel.channels.values()) {
+        delete c;
+    }
+    schema::channel.channels.clear();
     schema::channel.groups.clear();
 
     schema_initialized = false;
+}
+
+
+void resetChannels()
+{
+    done();
     init();
 
     QList<MachineLoader *> list = GetLoaders();

oscar/SleepLib/machine.cpp
@@ -120,7 +120,7 @@ Machine::Machine(Profile *_profile, MachineID id) : profile(_profile)
 Machine::~Machine()
 {
     saveSessionInfo();
-    qDebug() << "Destroy Machine" << info.loadername << hex << m_id;
+    //qDebug() << "Destroy Machine" << info.loadername << hex << m_id;
 }
 Session *Machine::SessionExists(SessionID session)
 {

oscar/SleepLib/profiles.cpp
@@ -36,7 +36,7 @@ Preferences *p_pref;
 Preferences *p_layout;
 Profile *p_profile;
 
-Profile::Profile(QString path)
+Profile::Profile(QString path, bool open)
   : is_first_day(true),
      m_opened(false)
 {
@@ -58,7 +58,9 @@ Profile::Profile(QString path)
     p_filename = p_path + p_name + STR_ext_XML;
     m_machlist.clear();
 
-    Open(p_filename);
+    if (open) {
+        Open(p_filename);
+    }
 
     Set(STR_GEN_DataFolder, QString("{home}/Profiles/{UserName}"));
 
@@ -80,13 +82,17 @@ Profile::Profile(QString path)
     session = new SessionSettings(this);
     general = new UserSettings(this);
 
-    OpenMachines();
-    m_opened=true;
+    if (open) {
+        OpenMachines();
+        m_opened=true;
+    }
 }
 
 Profile::~Profile()
 {
-    removeLock();
+    if (m_opened) {
+        removeLock();
+    }
 
     delete user;
     delete doctor;

oscar/SleepLib/profiles.h
@@ -43,8 +43,8 @@ class SessionSettings;
 class Profile : public Preferences
 {
   public:
-    //! \brief Constructor.. Does not open profile
-    Profile(QString path);
+    //! \brief Constructor.. Does not open profile in UI, but loads it from disk by default
+    Profile(QString path, bool open=true);
 
     virtual ~Profile();
 

oscar/SleepLib/schema.h
@@ -265,6 +265,7 @@ class ChannelList
 extern ChannelList channel;
 
 void init();
+void done();
 
 } // namespace schema
 

oscar/docs/channels.xml
@@ -11,9 +11,6 @@ Important: One id code per item, DO NOT CHANGE ID NUMBERS!!!
 
 
 
-  <channel id="0x111e" class="data" name="TestChan1" details="Debugging Channel #2" label="Test #1" unit="" color="pink"/>
-  <channel id="0x111f" class="data" name="TestChan2" details="Debugging Channel #2" label="Test #2" unit="" color="blue"/>
-
   <channel id="0x1160" class="data" name="RMS9_E01" details="RMS9 Empty 1" label="E01" unit="" color="black"/>
   <channel id="0x1161" class="data" name="RMS9_E02" details="RMS9 Empty 2" label="E02" unit="" color="black"/>
   <channel id="0x1162" class="data" name="SetPressure" details="Set Pressure" label="Pressure" unit="" color="black"/>
@@ -37,7 +34,8 @@ Important: One id code per item, DO NOT CHANGE ID NUMBERS!!!
  </group>
  <group name="RMS9">
   <!-- RESMED Settings -->
-  <channel id="0xe200" class="setting" scope="!session" name="RMS9Mode" details="PAP Mode" label="PAP Mode" type="integer" link="0x1200">
+  <!-- there was a link="0x1200" in the RMS9Mode channel, but it seems unlikely to have ever worked. -->
+  <channel id="0xe200" class="setting" scope="!session" name="RMS9Mode" details="PAP Mode" label="PAP Mode" type="integer">
    <Option id="0" value="CPAP"/>
    <Option id="1" value="Auto"/>
    <Option id="2" value="VPAP"/>

oscar/tests/prs1tests.cpp
@@ -18,8 +18,7 @@ static QString prs1OutputPath(const QString & inpath, const QString & serial, in
 
 void PRS1Tests::initTestCase(void)
 {
-    QString profile_path = TESTDATA_PATH "profile/";
-    Profiles::Create("test", &profile_path);
+    p_profile = new Profile(TESTDATA_PATH "profile/", false);
 
     schema::init();
     PRS1Loader::Register();
@@ -28,6 +27,8 @@ void PRS1Tests::initTestCase(void)
 
 void PRS1Tests::cleanupTestCase(void)
 {
+    delete p_profile;
+    p_profile = nullptr;
 }
 
 
@@ -348,6 +349,9 @@ void parseAndEmitChunkYaml(const QString & path)
             file.close();
         }
     }
+    
+    p_profile->removeMachine(m);
+    delete m;
 }
 
 void PRS1Tests::testChunksToYaml()

commit d4fd330a31456cfd86ea6faa349644eab22f80d7

Fix regression introduced by cb57643.
The Test1/2 channels were no longer initialized. As a result, all
Test1/2 data got merged into channel 0.

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -2713,6 +2713,9 @@ void PRS1Import::CreateEventChannels(const PRS1DataChunk* chunk)
 
 EventList* PRS1Import::GetImportChannel(ChannelID channel)
 {
+    if (!channel) {
+        qCritical() << this->sessionid << "channel in import table has not been added to schema!";
+    }
     EventList* C = m_importChannels[channel];
     if (C == nullptr) {
         C = session->AddEventList(channel, EVL_Event);

oscar/SleepLib/schema.cpp
@@ -338,8 +338,8 @@ void init()
 //  <channel id="0x111e" class="data" name="TestChan1" details="Debugging Channel #1" label="Test #1" unit="" color="pink"/>
 //  <channel id="0x111f" class="data" name="TestChan2" details="Debugging Channel #2" label="Test #2" unit="" color="blue"/>
 
-//  schema::channel.add(GRP_CPAP, ch=new Channel(CPAP_Test1 = 0x111e, DATA, MT_CPAP, SESSION, STR_GRAPH_TestChan1, QObject::tr("Debugging channel #1"), QObject::tr("Top secret internal stuff you're not supposed to see ;)"), QObject::tr("Test #1"),  QString(), INTEGER,  QColor("pink")));
-//  schema::channel.add(GRP_CPAP, ch=new Channel(CPAP_Test2 = 0x111f, DATA, MT_CPAP, SESSION, STR_GRAPH_TestChan2, QObject::tr("Debugging channel #2"), QObject::tr("Top secret internal stuff you're not supposed to see ;)"), QObject::tr("Test #2"),  QString(), INTEGER,  Qt::blue));
+    schema::channel.add(GRP_CPAP, ch=new Channel(CPAP_Test1 = 0x111e, DATA, MT_CPAP, SESSION, STR_GRAPH_TestChan1, QObject::tr("Debugging channel #1"), QObject::tr("Top secret internal stuff you're not supposed to see ;)"), QObject::tr("Test #1"),  QString(), INTEGER,  QColor("pink")));
+    schema::channel.add(GRP_CPAP, ch=new Channel(CPAP_Test2 = 0x111f, DATA, MT_CPAP, SESSION, STR_GRAPH_TestChan2, QObject::tr("Debugging channel #2"), QObject::tr("Top secret internal stuff you're not supposed to see ;)"), QObject::tr("Test #2"),  QString(), INTEGER,  Qt::blue));
 
     RMS9_E01 = schema::channel["RMS9_E01"].id();
     RMS9_E02 = schema::channel["RMS9_E02"].id();