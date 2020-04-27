cfca3781605cb6f40a2cf4da866508edf90f03c6
sawinglogz <3787776-sawinglogz@users.noreply.gitlab.com>
2020-04-27 14:12:16 -0400

    Move pressure chart class into its own files.

---
+++ oscar/Graphs/gPressureChart.h
@@ -0,0 +1,48 @@
+/* gPressureChart Header
+ *
+ * Copyright (c) 2020 The Oscar Team
+ * Copyright (C) 2011-2018 Mark Watkins <mark@jedimark.net>
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file COPYING in the main directory of the source code
+ * for more details. */
+
+#ifndef GPRESSURECHART_H
+#define GPRESSURECHART_H
+
+#include "gSessionTimesChart.h"
+
+class gPressureChart : public gSummaryChart
+{
+public:
+    gPressureChart();
+    virtual ~gPressureChart() {}
+
+    virtual Layer * Clone() {
+        gPressureChart * sc = new gPressureChart();
+        gSummaryChart::CloneInto(sc);
+        return sc;
+    }
+
+//    virtual void preCalc();
+    virtual void customCalc(Day *day, QVector<SummaryChartSlice> &slices) {
+        int size = slices.size();
+        float hour = day->hours(m_machtype);
+        for (int i=0; i < size; ++i) {
+            SummaryChartSlice & slice = slices[i];
+            SummaryCalcItem * calc = slices[i].calc;
+
+            calc->update(slice.value, hour);
+         }
+    }
+    virtual void afterDraw(QPainter &, gGraph &, QRectF);
+
+    virtual void populate(Day * day, int idx);
+
+    virtual QString tooltipData(Day * day, int idx) {
+        return day->getCPAPModeStr() + "\n" + day->getPressureSettings() + gSummaryChart::tooltipData(day, idx);
+    }
+
+};
+
+#endif // GPRESSURECHART_H
