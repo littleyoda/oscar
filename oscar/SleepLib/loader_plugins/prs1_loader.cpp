littleyoda/oscar

commit 8fd2840e15e701311ef39ff82cc394c1d318dbcc

Fix the PRS1 loader to fully import DS1 data after it skips DS2 data.
This was a very specific edge case: when there was a single new DS1
session, the error resulting from encountering DS2 data would make
OSCAR think that it had imported zero sessions, even though it had
half-imported the DS1 session. (Those sessions would be lost after
quitting OSCAR.)

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -737,7 +737,10 @@ int PRS1Loader::Open(const QString & selectedPath)
             qWarning() << "PRS1Loader::Open() called without a valid m_ctx object present";
             return 0;
         }
-        c += OpenMachine(machinePath);
+        int imported = OpenMachine(machinePath);
+        if (imported > 0) {  // don't let errors < 0 suppress subsequent successes
+            c += imported;
+        }
         m_ctx->FlushUnexpectedMessages();
     }
     return c;

Htmldocs/release_notes.html
@@ -19,6 +19,7 @@
       <li>[fix] Update copyright notices in html files.</li>
       <li>[fix] Correct value display on exact data points, and near session boundaries with drift.</li>
       <li>[fix] Link to translations in Credits and Release Notes html is now translated to selected language.</li>
+      <li>[fix] Continue to import DreamStation 1 data even if there is (unsupported) DreamStation 2 data on the SD card.</li>
     </ul>
     <p>
     <b>Changes and fixes in OSCAR v1.3.0</b>

commit 48130243ed99fcfdad24379c5d0d076909bb859c

Fix minor PRS1 regression introduced in 8fd2840.
Complete failures were incorrectly being treated as fully up to date.

oscar/SleepLib/loader_plugins/prs1_loader.cpp
@@ -779,16 +779,23 @@ int PRS1Loader::Open(const QString & selectedPath)
     // TODO: Loaders should return the set of machines during detection, so that Open() will
     // open a unique device, instead of surprising the user.
     int c = 0;
+    bool failures = false;
     for (auto & machinePath : machines) {
         if (m_ctx == nullptr) {
             qWarning() << "PRS1Loader::Open() called without a valid m_ctx object present";
             return 0;
         }
         int imported = OpenMachine(machinePath);
-        if (imported > 0) {  // don't let errors < 0 suppress subsequent successes
+        if (imported >= 0) {  // don't let errors < 0 suppress subsequent successes
             c += imported;
+        } else {
+            failures = true;
         }
         m_ctx->FlushUnexpectedMessages();
+    }
+    if (c == 0 && failures) {
+        // report an error when there were failures and no successess
+        c = -1;
     }
     return c;
 }