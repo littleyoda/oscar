f6cb6312f42ad96e83a44a5d30c1c167ba3e5ceb
LoudSnorer <Just4Me.1947@gmail.com>
2023-11-17 12:48:08 -0500

    setting the MAX using session->calcMax fix one issue, but it caused a changed of the device setting which is much worse. so related to hard coded settings

--- oscar/SleepLib/loader_plugins/resvent_loader.cpp
+++ oscar/SleepLib/loader_plugins/resvent_loader.cpp
@@ -324,7 +324,8 @@ void LoadEvents(const QString& session_folder_path, Session* session, const Usag
         UpdateEvents(event_type, events, session);
     });
     session->settings[CPAP_PressureMin] = 4.0;
-    session->settings[CPAP_PressureMax] = qMax(session->calcMax(CPAP_Pressure),session->calcMax(CPAP_IPAP));
+    session->settings[CPAP_PressureMax] = 20.0 ; // was qMax(session->calcMax(CPAP_Pressure),session->calcMax(CPAP_IPAP)); // this results in a change of device settings.
+
 }
 
 template <typename T>
