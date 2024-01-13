480fff35d9e952be128b156ac88d2ed085df49c5
LoudSnorer <Just4Me.1947@gmail.com>
2024-01-13 16:51:26 -0500

    added imissing Oscar copyright info

--- oscar/mytextbrowser.h
+++ oscar/mytextbrowser.h
@@ -1,4 +1,12 @@
-﻿#ifndef MYTEXTBROWSER_H
+﻿/* OSCAR 
+ *
+ * Copyright (c) 2019-2024 The OSCAR Team
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file COPYING in the main directory of the source code
+ * for more details. */
+
+#ifndef MYTEXTBROWSER_H
 #define MYTEXTBROWSER_H
 
 #include <QTextBrowser>
